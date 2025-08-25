0511eb1866f30f905f33b532b4a4720fb4bbc275
williamz-hub <moulong111@yahoo.co.jp>
2025-08-25 12:47:54 +0000

    初期ポモドーロタイマーアプリのファイルを追加

---
+++ main.py
@@ -0,0 +1,13 @@
+# Fibonacci数列を計算する関数
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n):
+            a, b = b, a + b
+        return b
